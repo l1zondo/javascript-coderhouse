76b2679a6dce4ac8500fbc9fac611797e5ef05ee
l1zondo <madlizondo@gmail.com>
2022-08-16 21:45:29 -0300

    agrego de cards DOM

--- js/carrito.js
+++ js/carrito.js
@@ -1,23 +1,8 @@
-const ropa=[
-    {
-        id:"camisa1",
-         titulo:"camisa vaquita",
-        precio:4500
-     },
-
-     {
-         id:"jean1",
-        titulo:"jean elastizado azul",
-         precio:5000
-     },
-    {
-        id:"remera1",
-         titulo:"remera emoji",
-         precio:2500
-     }
- ]
- console.log(ropa)
-
+console.dir(document.body)
+const productos=[{id: 1, titulo:"camisa vaquita", precio:4500},
+{id: 2, titulo:"jean elastizado azul", precio:5000},
+{id: 3, titulo:"remera emoji", precio:2500}];
+ console.log(productos)
 function calcularIva(precio){
     let precioConIva=precio * 1.21;
     return precioConIva;
@@ -32,6 +17,18 @@ function aplicarDescuento(precio){
 let precioConAumento=aplicarDescuento(calcularIva(parseInt(prompt("Ingrese los valores de lo que quiere comprar"))))
 console.log(precioConAumento);
 
- 
 
-
+let cartas=document.getElementById("cartas");
+for(const producto of productos){
+    let carta=document.createElement("div");
+    carta.className="card col-md-6";
+    carta.innerHTML=`
+    <div class="card-body">
+          <h5 class="card-title">${producto.titulo}</h5>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">COMPRAR</button>
+        </div>
+    
+    `;
+    cartas.append(carta);
+}
